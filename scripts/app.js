836d7518076679533aca879ac64751c7c8d948e6
lsandaruwanjayas <fgod406@gmail.com>
2023-01-26 11:00:42 +0530

    origin

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,26 @@
-/* 5. Get Weather API Call */
+/* 6. Updating the Location */
+
+const cityForm = document.querySelector('form');
+
+const updateCity = async (city) => {
+
+     const cityDets = await getCity(city);
+     const Weather = await getWeather(cityDets.Key);
+
+     return{
+        cityDets: cityDets,
+        weather: Weather
+     };
+};
+
+cityForm.addEventListener('submit', e => {
+
+    e.preventDefault();
+
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    updateCity(city)
+    .then( data => console.log(data))
+    .catch(err => console.log(err));
+});
