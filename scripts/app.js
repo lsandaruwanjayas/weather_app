7e413f44583f5c295d55a632ec6e1efb2b25bbf2
lsandaruwanjayas <fgod406@gmail.com>
2023-01-26 16:37:42 +0530

    origin

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,34 @@
-/* 6. Updating the Location */
+/* 8. Updating the UI */
 
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+
+    const cityDets = data.cityDets;
+    const Weather = data.Weather; 
+
+    details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="mu-3">${Weather.WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>${Weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+    </div>
+    `;
+};
+
+if(card.classList.contains('d-none')){
+    card.classList.remove('d-none');
+}
 
 const updateCity = async (city) => {
 
      const cityDets = await getCity(city);
      const Weather = await getWeather(cityDets.Key);
 
-     return{
-        cityDets: cityDets,
-        weather: Weather
-     };
+     return{ cityDets, Weather };
 };
 
 cityForm.addEventListener('submit', e => {
@@ -21,6 +39,6 @@ cityForm.addEventListener('submit', e => {
     cityForm.reset();
 
     updateCity(city)
-    .then( data => console.log(data))
+    .then( data => updateUI(data))
     .catch(err => console.log(err));
 });
